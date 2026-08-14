224825bc4a593c7a88feb7d17566b869b6332d2d
Shahid5261 <shahidkhan37090@gmail.com>
2026-08-14 22:04:42 +0530

    Fix production CORS

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.models_user import User
 from app.database import Base, engine
 from app.api.complaints import router as complaint_router
@@ -8,32 +9,40 @@ from app.api.auth import router as auth_router
 from app.api.dashboard_ai import router as dashboard_ai_router
 
 
-
 # Create database tables
 Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title="AI Complaint Management System",
     version="1.0.0"
 )
 
+
+# CORS configuration
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://cms-ai-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
+# API routers
 app.include_router(complaint_router)
 app.include_router(dashboard_router)
 app.include_router(auth_router)
 app.include_router(dashboard_ai_router)
+
+
 @app.get("/")
 def home():
     return {
         "message": "AI Complaint Management System API is running!"
     }
+
